34a6a014236206729f20ef9a4a340ba730012f1b
unknown <adrian.badsalt@gmail.com>
2023-08-09 13:58:54 +0700

    ganti route

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/** USERS */
-Route::get('/security/users', 'Security\UsersController@index');
-Route::get('/security/users/create', 'Security\UsersController@openForm');
-Route::post('/security/users/create', 'Security\UsersController@create');
-Route::get('/security/users/update', 'Security\UsersController@edit');
-Route::post('/security/users/update', 'Security\UsersController@update');
-Route::post('/security/users/destroy', 'Security\UsersController@destroy');
